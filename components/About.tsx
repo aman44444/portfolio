736f6c44afd48e232e4aab6935e43ad2b9fbc9f4
aman1357 <amancorp04@gmail.com>
2026-01-18 22:50:23 +0530

    style[AboutModal]: add close button border and section border

--- components/About.tsx
+++ components/About.tsx
@@ -14,31 +14,34 @@ const About = ({ isVisible, onClose }) => {
       id="wrapper"
     >
       <div className="relative md:mt-10 w-11/12 md:w-3/4 h-3/5 md:h-3/4 bg-white rounded-xl overflow-auto">
-        <div className="bg-gray-200 w-full h-1/4 rounded-t-xl text-2xl md:text-3xl flex items-center justify-between px-4 md:px-6 py-4">
+        <div className="w-full h-1/4 rounded-t-xl text-2xl md:text-3xl flex items-center justify-between px-4 md:px-6 py-4">
           <h2 className="font-semibold">About</h2>
           <button
-            className="text-black font-bold text-sm bg-white rounded-full h-8 w-8 flex items-center justify-center"
+            className="text-black font-bold text-sm border-gray-200 border-2 rounded-full h-8 w-8 flex items-center justify-center"
             onClick={() => onClose()}
             aria-label="Close"
           >
             X
           </button>
         </div>
+        <div className="w-full h-[1px] bg-gray-300"/>
+        <div className="w-full h-[1px] mt-[3px] bg-gray-300"/>
         <div className="flex flex-col items-start mt-4 md:mt-8 px-4 md:px-8">
-          <Image
+         <div className="flex gap-6">
+              <Image
             src="/assets/images/profilepicture.jpg"
             alt="Profile pic"
             width={100}
             height={100}
-            className="rounded-full mb-4"
+            className="rounded-md mb-4"
           />
-          <div>
-            <h3 className="text-lg md:text-xl font-semibold">Aman</h3>
-            <p className="text-gray-400 text-xs md:text-sm flex items-center">
-              <MdLocationOn className="text-black text-lg md:text-xl mr-1" />{" "}
+            <div>
+            <h3 className="text-lg md:text-xl font-semibold ml-[2px]">Aman</h3>
+            <p className="text-gray-400 text-xs md:text-sm flex items-center mt-[4px]">
+              <MdLocationOn className="text-black text-lg md:text-xl" />{" "}
               Bhopal, Madhya Pradesh
             </p>
-            <div className="flex gap-3 mt-2 md:mt-4">
+            <div className="flex gap-3 mt-3 md:mt-4">
               <a
                 href="https://github.com/aman44444"
                 target="_blank"
@@ -66,6 +69,7 @@ const About = ({ isVisible, onClose }) => {
               </a>
             </div>
           </div>
+         </div>
           <div className="mt-4 md:mt-8">
             <h3 className="text-lg md:text-xl font-semibold">Description</h3>
             <p className="text-gray-400 mt-2 text-sm md:text-base">
