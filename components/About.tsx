383da7874febe8a512e03662a8c60a4d055d24c1
aman1357 <amancorp04@gmail.com>
2026-01-20 20:51:23 +0530

    feat: add hover on close button

--- components/About.tsx
+++ components/About.tsx
@@ -17,59 +17,61 @@ const About = ({ isVisible, onClose }) => {
         <div className="w-full h-1/4 rounded-t-xl text-2xl md:text-3xl flex items-center justify-between px-4 md:px-6 py-4">
           <h2 className="font-semibold">About</h2>
           <button
-            className="text-black font-bold text-sm border-gray-200 border-2 rounded-full h-8 w-8 flex items-center justify-center"
+            className="text-black hover:bg-gray-200 font-bold text-sm border-2 rounded-full h-8 w-8 flex items-center justify-center"
             onClick={() => onClose()}
             aria-label="Close"
           >
             X
           </button>
         </div>
-        <div className="w-full h-[1px] bg-gray-300"/>
-        <div className="w-full h-[1px] mt-[3px] bg-gray-300"/>
+        <hr className="w-full h-[1px] bg-gray-300" />
+        <hr className="w-full h-[1px] bg-gray-300 mt-[3px]" />
         <div className="flex flex-col items-start mt-4 md:mt-8 px-4 md:px-8">
-         <div className="flex gap-6">
-              <Image
-            src="/assets/images/profilepicture.jpg"
-            alt="Profile pic"
-            width={100}
-            height={100}
-            className="rounded-md mb-4"
-          />
+          <div className="flex gap-6">
+            <Image
+              src="/assets/images/profilepicture.jpg"
+              alt="Profile pic"
+              width={100}
+              height={100}
+              className="rounded-md mb-4"
+            />
             <div>
-            <h3 className="text-lg md:text-xl font-semibold ml-[2px]">Aman</h3>
-            <p className="text-gray-400 text-xs md:text-sm flex items-center mt-[4px]">
-              <MdLocationOn className="text-black text-lg md:text-xl" />{" "}
-              Bhopal, Madhya Pradesh
-            </p>
-            <div className="flex gap-3 mt-3 md:mt-4">
-              <a
-                href="https://github.com/aman44444"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="GitHub"
-                className="text-xl"
-              >
-                <BsGithub />
-              </a>
-              <a
-                href=""
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Twitter"
-                className="text-xl"
-              >
-                <BsTwitter />
-              </a>
-              <a
-                href="mailto:amancorp04@gmail.com"
-                aria-label="Email"
-                className="text-xl"
-              >
-                <BiLogoGmail />
-              </a>
+              <h3 className="text-lg md:text-xl font-semibold ml-[2px]">
+                Aman
+              </h3>
+              <p className="text-gray-400 text-xs md:text-sm flex items-center mt-[4px]">
+                <MdLocationOn className="text-black text-lg md:text-xl" />{" "}
+                Bhopal, Madhya Pradesh
+              </p>
+              <div className="flex gap-3 mt-3 md:mt-4">
+                <a
+                  href="https://github.com/aman44444"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="GitHub"
+                  className="text-xl"
+                >
+                  <BsGithub />
+                </a>
+                <a
+                  href=""
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Twitter"
+                  className="text-xl"
+                >
+                  <BsTwitter />
+                </a>
+                <a
+                  href="mailto:amancorp04@gmail.com"
+                  aria-label="Email"
+                  className="text-xl"
+                >
+                  <BiLogoGmail />
+                </a>
+              </div>
             </div>
           </div>
-         </div>
           <div className="mt-4 md:mt-8">
             <h3 className="text-lg md:text-xl font-semibold">Description</h3>
             <p className="text-gray-400 mt-2 text-sm md:text-base">
