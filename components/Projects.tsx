e0d3a41248b19f2c70e715457af2889c1512b94c
aman1357 <amancorp04@gmail.com>
2026-01-30 21:51:37 +0530

    refactor[projects]: change data file name

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./Card";
+import Card from "./Card/Card";
 import { ProjectItem, ProjectModalProps } from "@/types/project";
 import Modal from "./Modal/Modal";
 
