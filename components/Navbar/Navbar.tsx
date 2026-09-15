5d3b8b82f213c481a816c51d00266f2cb9388e7c
aman1357 <amancorp04@gmail.com>
2026-09-15 17:39:50 +0530

    refactor: update code structure

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,36 +1,61 @@
 "use client";
-import React from "react";
+
 import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { IoIosArrowDown } from "react-icons/io";
+
 import About from "../About/About";
 import Project from "../Projects/Projects";
 import { PROJECTS } from "../../data/projectsData";
 import { ActiveModal } from "../../types/Modal";
+
+const navItems = [
+  { label: "About", modal: "about" as const },
+  { label: "Projects", modal: "projects" as const },
+];
+
+const navLinkClass =
+  "group relative text-lg text-gray-500 hover:text-gray-900";
+
+const underlineClass =
+  "absolute left-0 -bottom-1 h-[1px] w-0 bg-gray-900 " +
+  "transition-all duration-700 ease-in-out group-hover:w-full";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeModal, setActiveModal] = useState<ActiveModal>(null);
   const [pendingModal, setPendingModal] = useState<ActiveModal>(null);
 
-  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
-  const closeMenu = () => setIsMenuOpen(false);
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   const openModal = (modal: ActiveModal) => {
     setPendingModal(modal);
     closeMenu();
   };
 
-  const closeModal = () => setActiveModal(null);
+  const closeModal = () => {
+    setActiveModal(null);
+  };
 
   return (
     <header className="relative">
-      <div className="absolute w-full flex justify-center mt-10 md:mt-16">
+      {/* Menu toggle */}
+      <div className="absolute mt-10 flex w-full justify-center md:mt-16">
         <motion.div
           animate={{ rotate: isMenuOpen ? 180 : 0 }}
           transition={{ duration: 0.3 }}
         >
-          <button onClick={toggleMenu} aria-label="Toggle menu">
+          <button
+            type="button"
+            onClick={toggleMenu}
+            aria-label="Toggle menu"
+          >
             <IoIosArrowDown
               size={30}
               className="cursor-pointer text-gray-400 hover:text-gray-900"
@@ -39,6 +64,7 @@ const Navbar = () => {
         </motion.div>
       </div>
 
+      {/* Navigation */}
       <AnimatePresence
         onExitComplete={() => {
           if (pendingModal) {
@@ -49,80 +75,33 @@ const Navbar = () => {
       >
         {isMenuOpen && (
           <motion.nav
-            className="fixed top-0 left-0 z-40 w-full 
-                       flex flex-col items-center"
+            className="fixed left-0 top-0 z-40 flex w-full flex-col items-center"
             initial={{ y: "-100%" }}
             animate={{ y: 0 }}
             exit={{ y: "-100%" }}
             transition={{ duration: 0.7, ease: "easeInOut" }}
           >
             <ul className="flex gap-10 pt-3 pb-[3px] md:py-4">
-              <li>
-                <button
-                  onClick={() => openModal("about")}
-                  className="group relative text-lg text-gray-500 hover:text-gray-900"
-                >
-                  About
-                  <span
-                    className="
-          absolute
-          left-0
-          -bottom-1
-          h-[1px]
-          w-0
-          bg-gray-900
-          transition-all
-          duration-700
-          ease-in-out
-          group-hover:w-full
-        "
-                  />
-                </button>
-              </li>
-
-              <li>
-                <button
-                  onClick={() => openModal("projects")}
-                  className="group relative text-lg text-gray-500 hover:text-gray-900"
-                >
-                  Projects
-                  <span
-                    className="
-          absolute
-          left-0
-          -bottom-1
-          h-[1px]
-          w-0
-          bg-gray-900
-          transition-all
-          duration-700
-          ease-in-out
-          group-hover:w-full
-        "
-                  />
-                </button>
-              </li>
+              {navItems.map(({ label, modal }) => (
+                <li key={modal}>
+                  <button
+                    type="button"
+                    onClick={() => openModal(modal)}
+                    className={navLinkClass}
+                  >
+                    {label}
+                    <span className={underlineClass} />
+                  </button>
+                </li>
+              ))}
 
               <li>
                 <a
                   href="mailto:amancorp04@gmail.com"
-                  className="group relative text-lg text-gray-500 hover:text-gray-900"
+                  className={navLinkClass}
                 >
                   Contact
-                  <span
-                    className="
-          absolute
-          left-0
-          -bottom-1
-          h-[1px]
-          w-0
-          bg-gray-900
-          transition-all
-          duration-700
-          ease-in-out
-          group-hover:w-full
-        "
-                  />
+                  <span className={underlineClass} />
                 </a>
               </li>
             </ul>
@@ -130,7 +109,12 @@ const Navbar = () => {
         )}
       </AnimatePresence>
 
-      <About isOpen={activeModal === "about"} onClose={closeModal} />
+      {/* Modals */}
+      <About
+        isOpen={activeModal === "about"}
+        onClose={closeModal}
+      />
+
       <Project
         isOpen={activeModal === "projects"}
         onClose={closeModal}
@@ -141,3 +125,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
